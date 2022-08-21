7ac17854b77b055406c4003b6f0c437f2176b60e
cab <cab404@mailbox.org>
2022-08-21 23:26:03 +0400

    newer, badder tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 set -e
-
-cp ../examples/future-generations.md text.md
-
 cp ../examples/future-generations.md text.md
 cp ../examples/future-generations.json text.json
 
@@ -10,11 +7,9 @@ runCycle() {
     cargo run -- text.json text.md && mv new-text.json text.json
 }
 
-{ echo $'test\n\n'; cat text.md; } > text2.md; mv text2.md text.md
-runCycle
-
-{ echo $'test\n\n'; cat text.md; } > text2.md; mv text2.md text.md
-runCycle
-
-{ echo $'test\n\n'; cat text.md; } > text2.md; mv text2.md text.md
-runCycle
+for I in {1..10}; do
+  insert_loc=10
+  echo Cycle $I, inserting at $insert_loc
+  { head -n $insert_loc text.md; echo $'test\n\n'; tail -n +$insert_loc text.md; } > text2.md; mv text2.md text.md
+  runCycle
+done
